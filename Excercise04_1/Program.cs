416733524a3d7ab6410b45449b80e2a6885343bc
Arnonthawajjana <Arnonthawajjana@hotmail.com>
2017-04-16 13:20:03 +0700

    add count words from file and find duplicate from file

---
+++ Excercise04_1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise04_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* for detail of how difference file vs fileinfo */
+            /*https://stackoverflow.com/questions/1324788/what-is-the-difference-between-file-and-fileinfo-in-c*/
+            var currentfile = @"e:\testfile\textexample.txt";
+
+            while (true)
+            {
+                if (!Directory.Exists(Path.GetDirectoryName(currentfile)))
+                {
+                    Console.WriteLine("Directory does not Exists");
+                    break;
+                }
+                if (!File.Exists(currentfile))
+                {
+                    Console.WriteLine("File does not Exists");
+                    break;
+                }
+                else
+                { 
+                    Console.WriteLine(Path.GetDirectoryName(currentfile));
+                    Console.WriteLine(Path.GetFileName(currentfile));
+                    Console.WriteLine(Path.GetExtension(currentfile));
+                    Console.WriteLine("Have "+File.ReadAllText(currentfile).Split(' ').Length+" Words.");
+                    Console.ReadLine();
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Excercise04_2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise04_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* for detail of how difference file vs fileinfo */
+            /*https://stackoverflow.com/questions/1324788/what-is-the-difference-between-file-and-fileinfo-in-c*/
+            var currentfile = @"e:\testfile\textexample.txt";
+
+            while (true)
+            {
+                /* old version
+                List<String> words = File.ReadAllText(currentfile).Split(' ').ToList();
+                var duplicateKeys = words.GroupBy(x => x)
+                    .Where(group => group.Count() > 1)
+                    .OrderBy(group => group.Count())
+                    .Select(group => new {word = group.Key,times = group.Count()})
+                    .Take(3);
+                */
+                /* New version with linq */
+                var duplicateKeys = File.ReadAllText(currentfile).Split(' ').ToList().GroupBy(x => x)
+                   .Where(group => group.Count() > 1)
+                   .OrderBy(group => group.Key).ThenBy(group => group.Count())
+                   .Select(group => new { word = group.Key, times = group.Count()})
+                   .Take(3);
+
+                foreach (var s in duplicateKeys)
+                    Console.WriteLine(s);
+
+                Console.ReadLine(); 
+
+
+            }
+        }
+    }
+}
